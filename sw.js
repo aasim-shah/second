a552b8bf86e4547b77888b2efbb0513b102ff366
Aasim shah <asimshah8110@gmail.com>
2021-08-30 23:34:21 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,3 +5,12 @@ self.addEventListener("install", e => {
         })
     );
 });
+
+
+self.addEventListener("fetch" , e => {
+    e.respondWait(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
