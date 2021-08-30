c9ca1e611f9bd81a96dde71bebdc3218a1e5bc24
Aasim shah <asimshah8110@gmail.com>
2021-08-30 23:12:29 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 self.addEventListener("install", e => {
-e.WaitUntil(
+
     caches.open("static").then(cache =>{
         return cache.addAll(["." , "/images/logo192.png"]);
-      })
+      });
     
-);
+
 });
